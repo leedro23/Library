daa7a1ad50c4286954f9340b8510a06174cf7f89
Mario Ferreira <mariosilvaosbourne@gmail.com>
2019-09-20 22:27:32 -0300

    Criando interface

---
+++ src/impl/ILivroRepository.java
@@ -0,0 +1,9 @@
+package impl;
+
+import model.Livro;
+
+public interface ILivroRepository {
+	
+	public void livroAdd(Livro livro);
+
+}
---
+++ src/repository/LivroRepository.java
@@ -0,0 +1,14 @@
+package repository;
+
+import impl.ILivroRepository;
+import model.Livro;
+
+public class LivroRepository implements ILivroRepository{
+
+	@Override
+	public void livroAdd(Livro livro) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
